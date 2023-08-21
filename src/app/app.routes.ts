d9246f3d75d91569f16e6f9d2266290e250e6dbc
Candido Hernandez <candidohm99cursos@gmail.com>
2023-08-21 12:02:07 -0600

    correccion de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,12 +8,14 @@ import { HeroeComponent } from './components/heroe/heroe.component';
 import { BuscadorComponent } from './components/buscador/buscador.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent},
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'heroes', component: HeroesComponent },
   { path: 'heroe/:id', component: HeroeComponent},
   { path: 'buscar/:termino', component: BuscadorComponent},
-  { path: '**', pathMatch: 'full', redirectTo: 'home'}
+  { path: '**', pathMatch: 'full', redirectTo: 'home'},
+  { path: '**', component: HomeComponent}
 ];
 
 @NgModule({
